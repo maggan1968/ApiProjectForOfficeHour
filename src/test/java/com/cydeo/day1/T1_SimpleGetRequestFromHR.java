df13cd528761cf323a35fe285cbc5deb9a2c22c3
Malgorzata Altunsayar <mkowalczyk42@gmail.com>
2022-04-27 12:50:22 -0500

    Initial commit

---
+++ src/test/java/com/cydeo/day1/T1_SimpleGetRequestFromHR.java
@@ -0,0 +1,30 @@
+package com.cydeo.day1;
+
+import org.junit.jupiter.api.Test;
+
+public class T1_SimpleGetRequestFromHR {
+
+    /*
+    task 1:
+
+1. Send request to HR url and save the response
+2. GET /regions
+3. Store the response in Response Object that comes from get Request
+4. Print out followings
+- Headers
+Content-Type
+Status Code
+Response
+- Date
+5.
+Verify response body has
+"Europe"
+6. Verify response has Date
+
+     */
+
+    @Test
+    public void test1(){
+        
+    }
+}
